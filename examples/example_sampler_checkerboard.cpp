47b520f1a13fa007a6195eca8e3b6736c01921dc
tomer <tomer.shalev@gmail.com>
2021-04-01 16:55:05 +0300

    wip

--- examples/example_sampler_checkerboard.cpp
+++ examples/example_sampler_checkerboard.cpp
@@ -10,20 +10,20 @@
 using namespace microgl::sampling;
 
 int main() {
-    using Canvas24= canvas<bitmap<coder::RGB888_PACKED_32>, CANVAS_OPT_32_BIT>;
+    using Canvas24= canvas<bitmap<coder::RGB888_PACKED_32>>;
     using number = float;
 //    using number = Q<12>;
 
     Canvas24 canvas(W, H);
     checker_board<> sampler{{0, 0, 0, 255},
                             {255, 255, 255, 255},
-                            2, 2};
+                            10, 10};
 
     checker_board_pot<1,1> sampler_pot{{0, 0, 0, 255},
                             {255, 255, 255, 255}};
 
     auto render = [&]() -> void {
-        canvas.clear({255,0,255,255});
+        canvas.clear({255,200,255,255});
         canvas.drawRect<blendmode::Normal, porterduff::None<>, false, number>(
                 sampler,
                 0, 0, 300, 300);
