ed7252099687989a41cd25093e225e54691e7a5b
tomer <tomer.shalev@gmail.com>
2021-04-26 01:13:15 +0300

    added new rect sampler

---
+++ examples/example_sampler_white_noise.cpp
@@ -0,0 +1,33 @@
+#include "src/example.h"
+#include <microgl/canvas.h>
+#include <microgl/pixel_coders/RGB888_PACKED_32.h>
+#include <microgl/samplers/white_noise_sampler.h>
+
+#define W 640
+#define H 640
+
+using namespace microgl;
+using namespace microgl::sampling;
+
+int main() {
+    using Canvas24= canvas<bitmap<coder::RGB888_PACKED_32>>;
+    using number = float;
+//    using number = Q<12>;
+
+    white_noise_sampler<4> sampler{};
+    Canvas24 canvas(W, H);
+
+
+    auto render = [&]() -> void {
+        static long long counter = 0;
+        if(counter++%50==0)
+            sampler.update();
+        canvas.clear({255,0,255,255});
+        canvas.drawRect<blendmode::Normal, porterduff::None<>, false, number>(
+                sampler,
+                0, 0, 250, 250);
+    };
+
+    example_run(&canvas, render, 100);
+}
+
