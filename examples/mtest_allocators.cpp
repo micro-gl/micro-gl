0fa7551f00571b96110bf9d7b9a42dadefaac1ac
tomer <tomer.shalev@gmail.com>
2021-06-13 20:11:47 +0300

    added stack allocator

--- examples/mtest_allocators.cpp
+++ examples/mtest_allocators.cpp
@@ -3,8 +3,40 @@
 #include <microgl/allocators/dynamic_allocator.h>
 #include <microgl/allocators/pool_allocator.h>
 #include <microgl/allocators/linear_allocator.h>
+#include <microgl/allocators/stack_allocator.h>
 #include <iostream>
 #include <new>
+
+void test_stack_allocator() {
+    using byte= unsigned char;
+    const int size = 5000;
+    byte memory[size];
+
+    stack_allocator<> alloc{memory, size};
+
+    void * a1 = alloc.allocate(5000);
+    void * a2 = alloc.allocate(512);
+    void * a3 = alloc.allocate(256);
+    void * a4 = alloc.allocate(128);
+    void * a5 = alloc.allocate(3);
+    alloc.free(a5);
+    alloc.free(a4);
+    alloc.free(a3);
+    alloc.free(a2);
+    alloc.free(a2);
+    alloc.free(a1);
+    alloc.free(a1);
+    //    void * a4 = alloc.allocate(200);
+    //    void * a5 = alloc.allocate(200);
+    //    void * a6 = alloc.allocate(200);
+    //    alloc.free(a2);
+    //    alloc.free(a4);
+    //    alloc.free(a6);
+    //    alloc.free(a3);
+    //    alloc.free(a3);
+    //    alloc.free(a3);
+
+}
 
 void test_dynamic_allocator() {
     using byte= unsigned char;
@@ -78,7 +110,8 @@ void test_linear_allocator() {
 
 
 int main() {
-    test_dynamic_allocator();
+    test_stack_allocator();
+//    test_dynamic_allocator();
 //    test_pool_allocator();
 //    test_linear_allocator();
 }
