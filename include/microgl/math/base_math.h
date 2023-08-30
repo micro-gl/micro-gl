15da7eef82e160a7069f3fa228f469b49ea61bee
Tomer Shalev <tomer.shalev@gmail.com>
2023-08-30 19:17:03 +0300

    test msbuild error

--- include/microgl/math/base_math.h
+++ include/microgl/math/base_math.h
@@ -46,7 +46,7 @@ namespace microgl {
         number sqrt_cpu(const number val, const number epsilon) {
             int ix = 0;
             number x = val, y = number(1);
-            while ((abs<number>(x - y) > epsilon) and (ix++<10)) {
+            while ((abs<number>(x - y) > epsilon) && (ix++<10)) {
                 x = (x + y) / number(2);
                 y = val / x;
             }
